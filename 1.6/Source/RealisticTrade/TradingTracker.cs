aaed317e8a2f1796998a39180ccec4727483b737
Taranchuk <nfhfyxer@gmail.com>
2026-01-06 13:45:22 +0200

    mod update + possible lag fix

---
+++ 1.6/Source/RealisticTrade/TradingTracker.cs
@@ -0,0 +1,116 @@
+using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RealisticTrade
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class HotSwappableAttribute : Attribute
+    {
+    }
+    [HotSwappable]
+    public class TradingTracker : MapComponent
+    {
+        public Dictionary<Faction, bool> factionsCanArrive = new Dictionary<Faction, bool>();
+        public Dictionary<Faction, float> factionSelectionWeight = new Dictionary<Faction, float>();
+
+        private List<(Settlement settlement, float daysToArrive)> friendlySettlementsNearby = new List<(Settlement settlement, float daysToArrive)>();
+        private int lastNearbySettlementCheckTick;
+        public TradingTracker(Map map) : base(map)
+        {
+
+        }
+
+        public async override void MapComponentTick()
+        {
+            base.MapComponentTick();
+            if (map.IsPlayerHome && Find.TickManager.TicksGame % 60000 == 0)
+            {
+                var result = await Task.Run(() =>
+                {
+                    return CalculateFriendlySettlementsNearby();
+                });
+                friendlySettlementsNearby = result;
+                lastNearbySettlementCheckTick = Find.TickManager.TicksGame;
+            }
+        }
+
+        public int lastLogTime = -1;
+        public float GetTradeIncidentSpawnOrCountModifier()
+        {
+            var count = this.FriendlySettlementsNearby().Count;
+            var season = GenLocalDate.Season(map.Tile);
+            var mapWealth = this.map.wealthWatcher.WealthTotal;
+
+            var modifier = RealisticTradeMod.settings.totalSettlementCountBonusCurve.Evaluate(count);
+            if (RealisticTradeMod.settings.scaleValuesByWorldSize)
+            {
+                modifier *= RealisticTradeMod.settings.worldSizeModifiersCurve.Evaluate(Find.World.PlanetCoverage);
+            }
+            modifier *= RealisticTradeMod.settings.seasonImpactBonusCurve.Evaluate((int)season);
+            modifier *= RealisticTradeMod.settings.colonyWealthAttractionBonusCurve.Evaluate(mapWealth);
+            if (lastLogTime == -1 || Find.TickManager.TicksGame - lastLogTime >= GenDate.TicksPerDay)
+            {
+                lastLogTime = Find.TickManager.TicksGame;
+                Log.Message($"FINAL_TRADER_PER_YEAR map wealth in {this.map} is {mapWealth}, factionWeight: {RealisticTradeMod.settings.colonyWealthAttractionBonusCurve.Evaluate(mapWealth)}");
+                Log.Message($"FINAL_TRADER_PER_YEAR Count of neutral/ally bases (faction relatinship is >=0) around {this.map} is {count}, factionWeight: {RealisticTradeMod.settings.totalSettlementCountBonusCurve.Evaluate(count)}");
+                Log.Message($"FINAL_TRADER_PER_YEAR Season is {season}, factionWeight: {RealisticTradeMod.settings.seasonImpactBonusCurve.Evaluate((int)season)}");
+            }
+            return modifier;
+        }
+        public List<(Settlement settlement, float daysToArrive)> FriendlySettlementsNearby()
+        {
+            if (friendlySettlementsNearby != null && lastNearbySettlementCheckTick > 0)
+            {
+                return friendlySettlementsNearby;
+            }
+            friendlySettlementsNearby = CalculateFriendlySettlementsNearby();
+            lastNearbySettlementCheckTick = Find.TickManager.TicksGame;
+            return friendlySettlementsNearby;
+        }
+
+        private List<(Settlement settlement, float daysToArrive)> CalculateFriendlySettlementsNearby()
+        {
+            var result = new List<(Settlement settlement, float daysToArrive)>();
+            Predicate<Settlement> validator = delegate (Settlement x)
+            {
+                if (x.Faction == map.ParentFaction)
+                {
+                    return false;
+                }
+                if (x.Faction.HostileTo(map.ParentFaction))
+                {
+                    return false;
+                }
+                return true;
+            };
+
+            var settlements = Find.World.worldObjects.SettlementBases.Where(x => validator(x)).ToList();
+            int maxDist = Mathf.CeilToInt(RealisticTradeMod.settings.maxTravelDistancePeriodForTrading * 60000f / 3300f);
+
+            foreach (var settlement in settlements)
+            {
+                if (Find.WorldGrid.ApproxDistanceInTiles(map.Tile, settlement.Tile) > maxDist)
+                {
+                    continue;
+                }
+
+                int traversalDistance = Find.WorldGrid.TraversalDistanceBetween(settlement.Tile, map.Tile, passImpassable: true, maxDist: maxDist);
+                if (traversalDistance != int.MaxValue && traversalDistance > 0)
+                {
+                    float daysToArrive = traversalDistance * 3300f / 60000f;
+                    if (daysToArrive > 0 && daysToArrive <= RealisticTradeMod.settings.maxTravelDistancePeriodForTrading)
+                    {
+                        result.Add((settlement, daysToArrive));
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
